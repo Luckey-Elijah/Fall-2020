c32997a2d23c40cdc67949893e1bb07b6debcee1
Elijah Luckey <luckeyelijah112@gmail.com>
2020-09-16 08:40:32 -0400

    each key is move to new line of map

--- Applied-Cryptography-FA-2020-CIS-4362-01/Homework-01/ceasar.py
+++ Applied-Cryptography-FA-2020-CIS-4362-01/Homework-01/ceasar.py
@@ -1,7 +1,31 @@
 caesar_map = {
-    ' ': ' ', 'A': 'X', 'B': 'Y', 'C': 'Z', 'D': 'A', 'E': 'B', 'F': 'C', 'G': 'D', 'H': 'E', 'I': 'F',
-    'J': 'G', 'K': 'H', 'L': 'I', 'M': 'J', 'N': 'K', 'O': 'L', 'P': 'M', 'Q': 'N', 'R': 'O', 'S': 'P',
-    'T': 'Q', 'U': 'R', 'V': 'S', 'W': 'T', 'X': 'U', 'Y': 'V', 'Z': 'W'
+    ' ': ' ',
+    'A': 'X',
+    'B': 'Y',
+    'C': 'Z',
+    'D': 'A',
+    'E': 'B',
+    'F': 'C',
+    'G': 'D',
+    'H': 'E',
+    'I': 'F',
+    'J': 'G',
+    'K': 'H',
+    'L': 'I',
+    'M': 'J',
+    'N': 'K',
+    'O': 'L',
+    'P': 'M',
+    'Q': 'N',
+    'R': 'O',
+    'S': 'P',
+    'T': 'Q',
+    'U': 'R',
+    'V': 'S',
+    'W': 'T',
+    'X': 'U',
+    'Y': 'V',
+    'Z': 'W'
 }
 
 test = 'DORIAN STRUCK THE NORTHERN BAHAMAS AS A CATEGORY FIVE HURRICANE'
