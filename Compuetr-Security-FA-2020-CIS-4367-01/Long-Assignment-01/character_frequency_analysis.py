b49de008006b0fa4f9382b179b019941753b7e8e
Elijah Luckey <luckeyelijah112@gmail.com>
2020-09-28 21:05:24 -0400

    Addin cipher and freq analysis

--- Compuetr-Security-FA-2020-CIS-4367-01/Long-Assignment-01/character_frequency_analysis.py
+++ Compuetr-Security-FA-2020-CIS-4367-01/Long-Assignment-01/character_frequency_analysis.py
@@ -1,49 +1,36 @@
-char_map_template = {
-    'A': 0,
-    'B': 0,
-    'C': 0,
-    'D': 0,
-    'E': 0,
-    'F': 0,
-    'G': 0,
-    'H': 0,
-    'I': 0,
-    'J': 0,
-    'K': 0,
-    'L': 0,
-    'M': 0,
-    'N': 0,
-    'O': 0,
-    'P': 0,
-    'Q': 0,
-    'R': 0,
-    'S': 0,
-    'T': 0,
-    'U': 0,
-    'V': 0,
-    'W': 0,
-    'X': 0,
-    'Y': 0,
-    'Z': 0,
-}
+from text_request import OnlineResource
 
-test = "UZQSOVUOHXMOPVGPOZPEVSGZWSZOPFPESXUDBMETSXAIZ" + \
-    "VUEPHZHMDZSHZOWSFPAPPDTSVPQUZWYMXUZUHSX" + \
-    "EPYEPOPDZSZUFPOMBZWPFUPZHMDJUDTMOHMQ"
+
+def check_unicode(char):
+    '''
+    Checks is char is in range of [a-zA-Z] and returns uppercase [char, True].
+    Otherwise it returns [None, False].
+    '''
+    unicode = ord(char)
+    # [A-Z] codes
+    if unicode >= 65 and unicode <= 90:
+        return True, char
+    # [a-z] codes
+    if unicode >= 97 and unicode <= 122:
+        return True, char.upper()
+    return False, None
 
 
 def count_letters(string):
     '''
-    Counts the occurrences of each character in the string.
+    Counts the  occurrences of each character in the string.
     Returns the result as an unsorted map.
     '''
     freq_map = {}
-    for letter in string:
-        if letter in freq_map:
-            # increment the count by one
-            freq_map[letter] += 1
-        else:
-            freq_map[letter] = 1
+    for character in string:
+        is_char_alpha, character = check_unicode(character)
+        if is_char_alpha:
+            if character in freq_map:
+                # increment the count by one
+                freq_map[character] += 1
+            else:
+                # adds the new character to the map
+                freq_map[character] = 1
     return freq_map
 
 
@@ -59,10 +46,13 @@ def map_to_freq(letter_map, string_length):
     for letter in letter_map:
         # calculates the frequency
         freq = round(letter_map[letter] / string_length * 100, 2)
-        print('%c: %5.2f (%d)' % (letter, freq, letter_map[letter]))
+        print('%c: %5.2f (%d), %s' %
+              (letter, freq, letter_map[letter], ord(letter)))
 
 
 if __name__ == "__main__":
-    string = test
+    online_resource = OnlineResource(
+        "https://www.gutenberg.org/files/100/100-0.txt")
+    string = online_resource.text
     freq_map = count_letters(string)
     map_to_freq(freq_map, len(string))
--- Compuetr-Security-FA-2020-CIS-4367-01/Long-Assignment-01/vigenere_cipher.py
+++ Compuetr-Security-FA-2020-CIS-4367-01/Long-Assignment-01/vigenere_cipher.py
@@ -1,8 +1,5 @@
 import sys
 
-alphabet_to_val = {}
-alphabet_list = []
-
 
 def gen_full_key(plaintext, key):
     '''
@@ -19,11 +16,32 @@ def encrypt_letter(char, key):
     '''
     E = (P + K) mod 26
     '''
-    p = alphabet_to_val[char]
-    k = alphabet_to_val[key]
+    # TODO: use all characters
+    p = ord(char)
+    k = ord(key)
     e = (p + k) % 26
-    char = alphabet_list[e]
-    return char
+
+    while e < 65:
+        e = e + 26
+    while e > 122:
+        e = e - 26
+    enc_char = chr(e)
+    return enc_char
+
+
+def decrypt_letter(char, key):
+    '''
+    P = (E - K + 26) mod 26
+    '''
+    e = ord(char)
+    k = ord(key)
+    p = (e - k + 26) % 26
+
+    while p < 65:
+        p = p + 26
+    while p > 122:
+        p = p + 26
+    return chr(p)
 
 
 def encrypt(plaintext, key):
@@ -33,24 +51,50 @@ def encrypt(plaintext, key):
     and the `key` to encrypt it.
     '''
     # Cipher starts empty.
-    cipher = ''
+    cipher_str = ''
 
     # Gets the *full key* (as in length).
     key = gen_full_key(plaintext, key)
-
-    for letter_index in range(len(plaintext)):
+    plaintext = plaintext.upper()
+    for char_index in range(len(plaintext)):
         # Obtain just the letter of the given iteration.
-        key_l = key[letter_index]
-        plaintext_l = plaintext[letter_index]
+        key_char = key[char_index]
+        plaintext_char = plaintext[char_index]
 
         # Sends the letter of this `letter_index` to `encrypt_letter`.
-        letter = encrypt_letter(plaintext_l, key_l)
+        cipher_char = encrypt_letter(plaintext_char, key_char)
 
         # Append to the new *encrypted* letter to the cipher.
-        cipher = cipher + letter
+        cipher_str = cipher_str + cipher_char
 
-    return cipher
+    return cipher_str
+
+
+def decrypt(ciphertext, key):
+    plaintext_str = ''
+
+    key = gen_full_key(ciphertext, key)
+    ciphertext = ciphertext.upper()
+
+    for char_index in range(len(ciphertext)):
+        # get the current values at the current index
+        key_char = key[char_index]
+        ciphertext_char = ciphertext[char_index]
+
+        # decrypt the current index key
+        plaintext_char = decrypt_letter(ciphertext_char, key_char)
+
+        # append the decrypt character to the string
+        plaintext_str = plaintext_str + plaintext_char
+
+    return plaintext_str
 
 
 if __name__ == "__main__":
-    print("Hello, World!")
+    plaintext = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+    key = 'HELP'
+    print('PLAINTEXT: {}\n'.format(plaintext))
+    ciphertext = encrypt(plaintext, key)
+    print('CIPHERTEXT: {}\n'.format(ciphertext))
+    decrypted_plaintext = decrypt(ciphertext, key)
+    print('DECRYPTED: {}\n'.format(decrypted_plaintext))
