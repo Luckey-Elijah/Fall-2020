6e515fa8e3ecf50ed48d93c03c99c994f4b980ae
Elijah Luckey <luckeyelijah112@gmail.com>
2020-10-02 22:06:26 -0400

    Updates to the the character analyzer

--- Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/character_frequency_analysis.py
+++ Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/character_frequency_analysis.py
@@ -1,7 +1,8 @@
-from text_request import OnlineResource
+from text_request import OnlineText
+import re
 
 
-def check_unicode(char):
+def __check_unicode(char) -> (bool, chr):
     '''
     Checks is char is in range of [a-zA-Z] and returns uppercase [char, True].
     Otherwise it returns [None, False].
@@ -16,43 +17,83 @@ def check_unicode(char):
     return False, None
 
 
-def count_letters(string):
+def count_letter_frequency(string, desc=True, include_spaces=False, alphanum_only=True) -> dict:
     '''
-    Counts the  occurrences of each character in the string.
+    Counts the occurrences of each character in the string.
     Returns the result as an unsorted map.
     '''
-    freq_map = {}
+    letter_map = {}
     for character in string:
-        is_char_alpha, character = check_unicode(character)
+        is_char_alpha, character = __check_unicode(character)
         if is_char_alpha:
-            if character in freq_map:
+            if character in letter_map:
                 # increment the count by one
-                freq_map[character] += 1
+                letter_map[character] += 1
             else:
                 # adds the new character to the map
-                freq_map[character] = 1
-    return freq_map
+                letter_map[character] = 1
+
+    # Sorts the dictionary
+    letter_map = {
+        letter: count
+        for letter, count in sorted(
+            letter_map.items(),
+            key=lambda letter_m: letter_m[1],
+            reverse=desc
+        )
+    }
+    return letter_map
 
 
-def map_to_freq(letter_map, string_length):
-    '''
-    Takes a letter map {(letter, count)} and
-    '''
-    # Orders the list
-    letter_map = {k: v for k, v in sorted(
-        letter_map.items(), key=lambda item: item[1], reverse=True)}
+class CharacterFrequencyAnalyzer():
+    def __init__(self, source_string, include_spaces=False, alphanum_only=True):
 
-    # prints the list
-    for letter in letter_map:
-        # calculates the frequency
-        freq = round(letter_map[letter] / string_length * 100, 2)
-        print('%c: %5.2f (%d), %s' %
-              (letter, freq, letter_map[letter], ord(letter)))
+        self.text = re.sub(r'\W+', '', source_string)
+        self.length = len(self.text)
+        self.map = count_letter_frequency(self.text)
+
+    def print_frequency(self):
+        """
+        Prints a table in this style: \n
+        ```txt
+        +--------+-----------+--------+
+        | letter | frequency | counts |
+        +--------+-----------+--------+
+        |      E |   8.36    | 481973 |
+        |      T |   6.14    | 354387 |
+        |      O |   5.77    | 333041 |
+        |      A |   5.38    | 310094 |
+        |   ect. |   X.XX    | ...... |
+        |   .... |   ....    | ...... |
+        ```
+        """
+
+        str_format = "| {0:>6} | {1:>6} | {2:^9.2f} |"
+        h_line = "+--------+--------+-----------+"
+        print(h_line)
+        print("| LETTER | COUNTS | FREQUENCY |")
+        print(h_line)
+
+        # prints the list
+        for letter in self.map:
+
+            # letter count
+            count = self.map[letter]
+
+            # this index freq
+            freq = round(count / self.length * 100, 2)
+            print(str_format.format(letter, count, freq))
+
+        print(h_line)
 
 
 if __name__ == "__main__":
-    online_resource = OnlineResource(
+    online_resource = OnlineText(
         "https://www.gutenberg.org/files/100/100-0.txt")
-    string = online_resource.text
-    freq_map = count_letters(string)
-    map_to_freq(freq_map, len(string))
+
+    if online_resource.status_code == 200:
+        freq_analysis = CharacterFrequencyAnalyzer(online_resource.text)
+        freq_analysis.print_frequency()
+        print(freq_analysis.length)
+    else:
+        print("Server response is not good.")
