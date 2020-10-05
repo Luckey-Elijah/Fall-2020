039cb89cf88e4506e2bdaf3c064c0b2d04f265e4
Elijah Luckey <luckeyelijah112@gmail.com>
2020-10-05 18:49:56 -0400

    updates text_resource to text

--- Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/text.py
+++ Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/text.py
@@ -13,3 +13,13 @@ class OnlineTextResource():
 
     def __init__(self, url):
         self.status_code, self.text = make_request(url)
+
+
+class LocalResource():
+    """
+    Used to read from a local text file resource. Has no dependencies.
+    """
+
+    def __init__(self, relative_path):
+        with open(relative_path, 'r') as f:
+            self.text = f.read()
