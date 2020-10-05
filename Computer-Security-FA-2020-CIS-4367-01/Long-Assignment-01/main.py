8d305e0712c6374a9ce3b2823be87748529e57e4
Elijah Luckey <luckeyelijah112@gmail.com>
2020-10-05 18:51:58 -0400

    clean up main.py

--- Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/main.py
+++ Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/main.py
@@ -1,23 +1,13 @@
 from character_frequency_analysis import CharacterFrequencyAnalyzer
-from text_request import OnlineTextResource
+from text import OnlineTextResource, LocalResource
 import sys
 
+if sys.version_info[0] < 3 and sys.version_info[1] < 6:
+    print("Python version is not supported. Please use version 3.6 or higher.")
+    exit()
+
 if __name__ == "__main__":
-    online_resource = OnlineTextResource(
-        "https://www.astrolog.org/labyrnth/novel.txt")
-
-    if online_resource.status_code == 200:
-        freq_analysis = CharacterFrequencyAnalyzer(online_resource.text)
-        freq_analysis.print_frequency()
-        print(freq_analysis.length)
-    else:
-        print("Server response is not good.")
-        print("Status Code: {0}".format(online_resource.status_code))
-
-    if sys.argv[1] == "file":
-        # TODO: file runner
-        pass
-    else:
-        # TODO: string runner
-        # use the argument as the string itself
-        pass
+    resource = LocalResource("example_literature/shakespeare.txt")
+    freq_analysis = CharacterFrequencyAnalyzer(resource.text)
+    freq_analysis.print_frequency()
+    print(freq_analysis.length)
