a2f267a20dbefe313b7fff8b3e24814e6a9c1df2
Elijah Luckey <luckeyelijah112@gmail.com>
2020-10-06 13:32:53 -0400

    adds dynamic support for custom alphabet

--- Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/vigenere_cipher.py
+++ Computer-Security-FA-2020-CIS-4367-01/Long-Assignment-01/vigenere_cipher.py
@@ -1,101 +1,215 @@
 import sys
 
 
-def gen_full_key(plaintext, key):
-    '''
-    Generates the full-length key give the `key` and `plaintext` cipher.
-    '''
-    if len(key) == len(plaintext):
-        return key
-    for letter in range(len(plaintext) - len(key)):
-        key = key + key[letter % len(key)]
-    return key
+def _assert_char_key_alphabet(char, key, alphabet) -> None:
+    # Assert char and key are the characters
+    if len(char) > 1 or len(key) > 1 or len(alphabet) != 26:
+        print("Please provide a single character. You provided:")
+        print("char: {}".format(char))
+        print("key:  {}".format(key))
+        raise AssertionError()
+    elif type(char) is not str or type(key) is not str or type(alphabet) is not str:
+        print("Not the correct type.")
+        raise TypeError()
 
 
-def encrypt_letter(char, key):
-    '''
+def _encrypt_letter(char, key, alphabet) -> str:
+    """
+    Encrypt an individual letter with a given `char`, `key`, and `alphabet`.
+    ```txt
     E = (P + K) mod 26
-    '''
-    # TODO: use all characters
-    p = ord(char)
-    k = ord(key)
-    e = (p + k) % 26
+    ```
+    """
 
-    # TODO: Test evaluations
-    while e < 65:
-        e = e + 26
-    while e > 122:
-        e = e - 26
-    enc_char = chr(e)
+    _assert_char_key_alphabet(char, key, alphabet)
+
+    if char not in alphabet:
+        return char
+
+    p = ord(char) - 65  # plaintext value
+    k = ord(key) - 65   # key value
+    e = (p + k) % 26    # encrypted value
+
+    enc_char = alphabet[e]
     return enc_char
 
 
-def decrypt_letter(char, key):
-    '''
+def _decrypt_letter(char, key, alphabet) -> str:
+    """
+    Decrypt an individual letter with a given `char`, `key`, and `alphabet`.
+    ```txt
     P = (E - K + 26) mod 26
-    '''
-    e = ord(char)
-    k = ord(key)
-    p = (e - k + 26) % 26
+    ```
+    """
 
-    while p < 65:
-        p = p + 26
-    while p > 122:
-        p = p + 26
+    _assert_char_key_alphabet(char, key, alphabet)
+
+    # Takes care of non alpha characters
+    if char not in alphabet:
+        return char
+
+    # a value of 0 - 25
+    e = alphabet.index(char)
+
+    # a value 0 - 25
+    k = ord(key) - 65
+
+    # p
+    p = (e - k + 26) % 26 + 65
+
     return chr(p)
 
 
-def encrypt(plaintext, key):
-    '''
-    Use this function to vigenere encrypt.
-    Pass a `plaintext` as the the message
-    and the `key` to encrypt it.
-    '''
-    # Cipher starts empty.
-    cipher_str = ''
+def _remove_non_alpha(text):
+    """
+    Removes non-alphabetic characters (including whitespace). [`a-zA-Z`].
+    """
+    # Assert text
+    if len(text) < 1 or type(text) is not str:
+        raise AssertionError()
 
-    # Gets the *full key* (as in length).
-    key = gen_full_key(plaintext, key)
-    plaintext = plaintext.upper()
-    for char_index in range(len(plaintext)):
-        # Obtain just the letter of the given iteration.
-        key_char = key[char_index]
-        plaintext_char = plaintext[char_index]
+    trimmed_text = ""
+    for letter in text:
+        if letter.isalpha():
+            trimmed_text = trimmed_text + letter
 
-        # Sends the letter of this `letter_index` to `encrypt_letter`.
-        cipher_char = encrypt_letter(plaintext_char, key_char)
-
-        # Append to the new *encrypted* letter to the cipher.
-        cipher_str = cipher_str + cipher_char
-
-    return cipher_str
+    return trimmed_text
 
 
-def decrypt(ciphertext, key):
-    plaintext_str = ''
+class VigenereCipher():
+    """
+    Used to perform vigenere encryption, vigenere decryption, and analysis on text.
+    `key` must be type `str`. Only use plaintext or cipher text. They will be written over.\n
+    ```python
+    v = VigenereCipher("key", plaintext="")
+    ```
+    """
 
-    key = gen_full_key(ciphertext, key)
-    ciphertext = ciphertext.upper()
+    def __init__(self, key, plaintext=None, cipher_text=None, alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
+        # Assert key given.
+        if (type(key) is not str):
+            print("Key type: {}".format(type(key)))
+            raise TypeError()
+        elif (len(key) < 3):
+            raise AssertionError()
+        else:
+            self.key = key
 
-    for char_index in range(len(ciphertext)):
-        # get the current values at the current index
-        key_char = key[char_index]
-        ciphertext_char = ciphertext[char_index]
+        # If init with values, assert and create them.
 
-        # decrypt the current index key
-        plaintext_char = decrypt_letter(ciphertext_char, key_char)
+        # plaintext
+        if plaintext and type(plaintext) is not str:
+            # When plaintext is given check its type
+            raise TypeError()
+        else:
+            self.plaintext = plaintext
 
-        # append the decrypt character to the string
-        plaintext_str = plaintext_str + plaintext_char
+        # cipher_text
+        if cipher_text and type(cipher_text) is not str:
+            # When cipher_text is given check its type
+            raise TypeError()
+        else:
+            self.cipher_text = cipher_text
 
-    return plaintext_str
+        # alphabet
+        if type(alphabet) is not str:
+            # When alphabet is given check its type
+            raise TypeError()
+        elif len(alphabet) != 26:
+            # When alphabet is given check its length
+            raise AssertionError()
+        else:
+            self.alphabet = alphabet
+
+    def _gen_full_key(self) -> str:
+        """
+        Generates the full-length key give the `key` and `plaintext` cipher.
+        """
+        # doesn't generate by len(self.cipher_text)
+        if self.plaintext and len(self.key) == len(self.plaintext):
+            return self.key
+        elif self.cipher_text and len(self.key) == len(self.cipher_text):
+            return self.key
+
+        key = self.key
+
+        if self.plaintext is not None:
+            for letter in range(len(self.plaintext) - len(self.key)):
+                key = key + key[letter % len(key)]
+            return key
+        elif self.cipher_text is not None:
+            for letter in range(len(self.cipher_text) - len(self.key)):
+                key = key + key[letter % len(key)]
+            return key
+        else:
+            raise AssertionError()
+
+    def encrypt(self, update_cipher_text=True) -> str:
+        """
+        Encrypts the `self.plaintext` field and returns the cipher text. Set `update_cipher=True` to update this (`self.cipher_text`) instance.
+        """
+
+        cipher_text = ""
+        full_key = self._gen_full_key()
+        plaintext = _remove_non_alpha(self.plaintext.upper())
+
+        for char_index in range(len(plaintext)):
+
+            # obtain just the letter of this iteration
+            key_char = full_key[char_index]
+            plntxt_char = plaintext[char_index]
+
+            # encrypts the single letter
+            ciph_char = _encrypt_letter(plntxt_char, key_char, self.alphabet)
+
+            # Append to the new *encrypted* letter to the cipher.
+            cipher_text = cipher_text + ciph_char
+
+        if update_cipher_text is True:
+            self.cipher_text = cipher_text
+
+        return cipher_text
+
+    def decrypt(self, update_plaintext=False) -> str:
+        """
+        Function that returns `str` of the decrypted ciphertext.
+        """
+
+        # Assert cipher text
+        if self.cipher_text is None:
+            raise AssertionError()
+
+        plaintext_str = ""
+        full_key = self._gen_full_key()
+        ciphertext = self.cipher_text.upper()
+
+        for char_index in range(len(ciphertext)):
+            key_char = full_key[char_index]
+            ciph_txt_char = ciphertext[char_index]
+
+            # decrypt the current index key
+            plntxt_char = _decrypt_letter(
+                ciph_txt_char, key_char, self.alphabet)
+
+            # append it to the plain text
+            plaintext_str = plaintext_str + plntxt_char
+
+        if update_plaintext is True:
+            self.plaintext = plaintext_str
+
+        return plaintext_str
 
 
 if __name__ == "__main__":
-    plaintext = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-    key = 'HELP'
-    print('PLAINTEXT: {}\n'.format(plaintext))
-    ciphertext = encrypt(plaintext, key)
-    print('CIPHERTEXT: {}\n'.format(ciphertext))
-    decrypted_plaintext = decrypt(ciphertext, key)
-    print('DECRYPTED: {}\n'.format(decrypted_plaintext))
+    # Test 1: Create cipher. Encrypt. Then decrypt back.
+
+    a = "3WLE4I9F,XGBDMUNR8.VT˚57YH"  # custom alphabet
+
+    plaintext = "my name is elijah, and I do not enjoy doing this. 1234567890"
+    v_1 = VigenereCipher("elijah", plaintext=plaintext, alphabet=a)
+    v_1.encrypt()  # CIPHER = 5NWA.8.S.3UJAYALX˚MOWOH87JOMXO.4T,M˚S
+
+    c_text = "5NWA.8.S.3UJAYALX˚MOWOH87JOMXO.4T,M˚S"
+
+    v_2 = VigenereCipher("elijah", cipher_text=c_text, alphabet=a)
+    v_2.decrypt(update_plaintext=True)  # MYNAMEISELIJAHANDIDONOTENJOYDOINGTHIS
